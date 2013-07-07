caa2c641ef180057c448f515d0bd69e0536457aa
e70838 <e70838@gmail.com>
2013-07-07 22:52:32 +0200

    Update 2 for part 1

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var fs = require('fs');
-var i = fs.readFileSync('index.html');
+var i = fs.readFileSync('index.html').toString('utf8');
 
 var app = express.createServer(express.logger());
 
